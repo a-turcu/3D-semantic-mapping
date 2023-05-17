811eed0c977370a11a48aec39217bc98cdfc4ffe
Alexandru Turcu <alexandru.turcu2000@gmail.com>
2023-05-17 16:53:18 +0200

    rotated env

--- src/solution/main.py
+++ src/solution/main.py
@@ -1,23 +1,28 @@
 import numpy as np
+import torch
 from PIL import Image
 from mmdet3d.apis import inference_detector, init_model
 import os
 
 from const import *
-from utils_pcd import create_pcd, load_pkl, vis_mm
-from iterative_global_registration import create_global_pointcloud
+from mmdet3d.structures import DepthInstance3DBoxes
+from mmdet3d.visualization import Det3DLocalVisualizer
+from utils_pcd import create_pcd, load_pkl, vis_mm, random_sampling
+import open3d as o3d
+from benchbot_eval.ground_truth_creator import GroundTruthCreator
+
 
 def load_data(filename):
-    depth_img = np.load(DEPTH_PATH + filename + ".npy")
-    rgb_img = Image.open(RGB_PATH  + filename + ".png")
+    depth_np = np.load(DEPTH_PATH + filename + ".npy")
+    rgb_img = Image.open(RGB_PATH + filename + ".png")
     rgb_np = np.asarray(rgb_img, dtype=np.float32)
-    info = load_pkl(INFO_PATH  + filename + ".pkl")
+    info = load_pkl(INFO_PATH + filename + ".pkl")
     intrinsics = info['matrix_intrinsics']
     intrinsics = intrinsics.reshape(9)
     intrinsics = intrinsics.astype(np.float32)
     poses = load_pkl(POSE_PATH + filename + ".pkl")
 
-    return depth_img, rgb_np, intrinsics, poses
+    return depth_np, rgb_np, intrinsics, poses
 
 
 def analyze_result(result, threshold=0):
@@ -45,12 +50,12 @@ def analyze_result(result, threshold=0):
         'night_stand', 'bookshelf', 'bathtub'
     ]
 
-    challenge_classes = classes_sunrgbd[:6]
-    challenge_classes[5] = 'table'
+    #challenge_classes = classes_sunrgbd[:6]
+    #challenge_classes[5] = 'table'
 
-    for i in labels_3d:
-        print(classes_sunrgbd[i])
-
+    for index, val in enumerate(labels_3d):
+        print(classes_sunrgbd[val], bboxes_3d[index])
+        print('\n')
 
 
 def generate_all_pcds():
@@ -65,19 +70,91 @@ def generate_all_pcds():
         create_pcd(depth_img, rgb_np, intrinsics, filename)
 
 
+def vis_mm_2(pcd, color=False, boxes=None):
+    visualizer = Det3DLocalVisualizer(pcd_mode=2)
+    if color:
+        visualizer.set_points(pcd, mode='xyzrgb')
+    else:
+        visualizer.set_points(pcd, mode='xyz')
+    if boxes is not None:
+        visualizer.draw_bboxes_3d(boxes)
+    visualizer.show()
+
+
+def transform_pcd(pcd):
+
+    # sample pcd for faster testing
+    pcd = random_sampling(pcd, 500_000)
+
+    # numpy to o3d
+    points = pcd[:, :3] / 1000
+    colors = pcd[:, 3:6] / 255.0
+    pcd_o3d = o3d.geometry.PointCloud()
+    pcd_o3d.points = o3d.utility.Vector3dVector(points)
+    pcd_o3d.colors = o3d.utility.Vector3dVector(colors)
+
+    # transform
+    R = pcd_o3d.get_rotation_matrix_from_xyz((0, 0, 0.63*np.pi))
+    pcd_o3d = pcd_o3d.rotate(R, center=(0, 0, 0))
+    pcd_o3d = pcd_o3d.translate((0.0011, 0.0012, 0.0012))
+    #pcd_o3d = pcd_o3d.scale(1, center=(0, 0, 0))
+
+    # o3d to numpy
+    pcd = np.asarray(pcd_o3d.points)
+    rgb_np = np.asarray(pcd_o3d.colors)
+    pcd = np.concatenate((pcd, rgb_np.reshape(-1, 3)), axis=1)
+    pcd = pcd.astype(np.float32)
+    pcd[:, :3] = pcd[:, :3] * 1000
+
+    return pcd
+
+
 def main():
 
     filename = "global_pcd"
+
     pcd_path = CLOUD_PATH + filename + ".npy"
-    if not os.path.exists(pcd_path):
-        create_global_pointcloud(pcd_path)
+    pcd = np.load(pcd_path)
+
+    pcd = transform_pcd(pcd)
+
+    transformed_path = CLOUD_PATH + "global_pcd_transformed.npy"
+    np.save(transformed_path, pcd)
 
     threshold = 0.7
     model = init_model(CONFIG_FILE, CHCKPOINT_FILE)
-    result, data = inference_detector(model, pcd_path)
+    result, data = inference_detector(model, transformed_path)
 
-    analyze_result(result, threshold)
+    # bed
+    box_bed = DepthInstance3DBoxes(torch.tensor([[2.20074341, 0.05944466, 0.44632973, 2.29851074, 1.85151764, 0.89577698, 0]]))
+
+    # sink
+    box_sink = DepthInstance3DBoxes(torch.tensor([[-0.58994232, 3.07765717, 0.82831787, 0.32866461999999996, 0.27531434, 0.14530166, 0]]))
+
+    # table 1
+    box_table1 = DepthInstance3DBoxes(torch.tensor([[0.47003494, -0.53999237, 0.17274715000000002, 0.60470368, 0.40477172000000006, 0.34549430000000003, 0]]))
+
+    # table 2
+    box_table2 = DepthInstance3DBoxes(torch.tensor([[3.11474518, 1.08274681, 0.17274715000000002, 0.40477112, 0.60470322, 0.34549430000000003, 0]]))
+
+    # center
+    box_center = DepthInstance3DBoxes(torch.tensor([[-0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0]]))
+
+    # result.pred_instances_3d.bboxes_3d.tensor[0, :] = box_bed.tensor
+    # result.pred_instances_3d.bboxes_3d.tensor[1, :] = box_sink.tensor
+    # result.pred_instances_3d.bboxes_3d.tensor[2, :] = box_table1.tensor
+    # result.pred_instances_3d.bboxes_3d.tensor[3, :] = box_table2.tensor
+    # result.pred_instances_3d.bboxes_3d.tensor[4:, :] = box_center.tensor
+    #
+    # result.pred_instances_3d.scores_3d[0] = 0.99
+    # result.pred_instances_3d.scores_3d[1] = 0.99
+    # result.pred_instances_3d.scores_3d[2] = 0.99
+    # result.pred_instances_3d.scores_3d[3] = 0.99
+
+
     vis_mm(model, data, result, threshold)
+    analyze_result(result, threshold)
+    print("ok")
 
 
 if __name__ == "__main__":
--- src/solution/testing.py
+++ src/solution/testing.py
@@ -1,21 +1,31 @@
 """
 !!! FILE CREATED FOR TESTING STUFF OUT !!!
 """
+import os
 
 import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 from scipy.spatial.transform import Rotation
+
 from mmdet3d.visualization.local_visualizer import Det3DLocalVisualizer
 from mmdet3d.registry import VISUALIZERS
 from mmdet3d.apis import inference_detector, init_model
-
-from main import analyze_result
-from utils_pcd import load_pkl, convert_depth_to_pointcloud, random_sampling
+#from mmdet.apis import init_detector, inference_detector
+#from mmdet.utils import register_all_modules
+#from mmdet.registry import VISUALIZERS
+import mmcv
+from main import load_data, vis_mm
+from utils_pcd import load_pkl, create_pcd, random_sampling
 from const import *
 
 
-def vis_mm_2(pcd, boxes=None):
+def vis_mm_2(pcd, color=False, boxes=None):
     visualizer = Det3DLocalVisualizer(pcd_mode=2)
-    visualizer.set_points(pcd, mode='xyz')
+    if color:
+        visualizer.set_points(pcd, mode='xyzrgb')
+    else:
+        visualizer.set_points(pcd, mode='xyz')
     if boxes is not None:
         visualizer.draw_bboxes_3d(boxes)
     visualizer.show()
@@ -39,44 +49,288 @@ def compute_world_T_camera(pose_rotation, pose_translation):
     rotation_matrix = np.eye(4)
     rotation_matrix[0:3][:, 0:3] = rot.as_dcm().T
 
-    # build the tranlation matrix
+    # build the translation matrix
     translation_matrix = np.eye(4)
     translation_matrix[3][0:3] = np.array(pose_translation)
 
-    # compute the tranformation matrix
+    # compute the transformation matrix
     transformation_matrix = rotation_matrix @ translation_matrix
 
     # # convert robot's right-handed coordinate system to unreal world's left-handed coordinate system
-    # transformation_matrix[:, 1] = -1.0 * transformation_matrix[:, 1]
+    #transformation_matrix[:, 1] = -1.0 * transformation_matrix[:, 1]
 
     return transformation_matrix
 
 
-# def camera_to_world_coord(boxes):
-#     # convert the 3D box corners in the camera coordinate system to the world coordinate system
-#     box_corners_camera = detection[1]
-#     box_corners_camera = np.concatenate(
-#         (box_corners_camera,
-#          np.ones((box_corners_camera.shape[0], 1),
-#                  dtype=box_corners_camera.dtype)),
-#         axis=1)
-#     box_corners_world = box_corners_camera @ w_T_c
-#     box_corners_world = box_corners_world[:, 0:3]
-#     result["box_corners_world"] = box_corners_world
-#     # compute the 3D box's center roughly
-#     box_center_world = np.sum(box_corners_world, axis=0) / 8.0
-#     result["centroid"] = box_center_world
-#     # compute the 3D box's extent roughly
-#     box_extent_world = np.sum(
-#         np.abs(box_corners_world - box_center_world), axis=0) / 4.0
-#     result["extent"] = box_extent_world
+def convert_box_to_world(box_corners_camera, w_T_c):
 
+    box_corners_camera = np.concatenate(
+        (box_corners_camera,
+         np.ones((box_corners_camera.shape[0], 1),
+                 dtype=box_corners_camera.dtype)),
+        axis=1)
+
+    box_corners_world = box_corners_camera @ w_T_c
+    box_corners_world = box_corners_world[:, 0:3]
+
+    # compute the 3D box's center roughly
+    box_center_world = np.sum(box_corners_world, axis=0) / 8.0
+
+    # compute the 3D box's extent roughly
+    box_extent_world = np.sum(
+        np.abs(box_corners_world - box_center_world), axis=0) / 4.0
+
+
+    return box_corners_world, box_center_world, box_extent_world
+
+
+def analyze_result(result, w_T_c, threshold=0):
+    """
+    Extracts, thresholds and prints the result of the inference
+    """
+
+    predictions = result.pred_instances_3d
+    scores_3d = predictions.scores_3d.cpu().numpy()
+    bboxes_3d = predictions.bboxes_3d
+    #box_centroids = bboxes_3d.corners.cpu().numpy()
+    labels_3d = predictions.labels_3d.cpu().numpy()
+
+    indices = []
+    for i, score in enumerate(scores_3d):
+        if score > threshold and labels_3d[i] < 6:
+            indices.append(i)
+
+    scores_3d = scores_3d[indices]
+    bboxes_3d = bboxes_3d[indices]
+    labels_3d = labels_3d[indices]
+
+    box_corners = bboxes_3d.corners.cpu().numpy()
+
+    # SUNRGBD classes
+    # TODO move somewhere else
+    classes_sunrgbd = [
+        'bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser',
+        'night_stand', 'bookshelf', 'bathtub'
+    ]
+
+    #challenge_classes = classes_sunrgbd[:6]
+    #challenge_classes[5] = 'table'
+
+
+    frame_results = []
+    for index, val in enumerate(labels_3d):
+        #transformed_bbox = convert_box_to_world(box_centroids[index], w_T_c)
+        #print(classes_sunrgbd[val])
+        corners, center, extent = convert_box_to_world(box_corners[index], w_T_c)
+        result = {
+            "class": classes_sunrgbd[labels_3d[index]],
+            "class_ID": labels_3d[index],
+            "confidence": scores_3d[index],
+            "box_corners_camera": box_corners[index],
+            "box_corners_world": corners,
+            "centroid": center,
+            "extent": extent
+        }
+        frame_results.append(result)
+
+    return frame_results
+
+
+def votenet_nms(all_results, class_list):
+    nms_iou_cls = 0.1  # this threshold for removing duplicates that are of the same class
+    boxes_world = []
+    results_list = []
+    for results in all_results:
+        if not results:
+            continue
+        for result in results:
+            box_world = np.zeros((1, 8), dtype=np.float32)
+            box_corners_world = result["box_corners_world"]
+            box_world[0, 0] = np.min(box_corners_world[:, 0])
+            box_world[0, 1] = np.min(box_corners_world[:, 1])
+            box_world[0, 2] = np.min(box_corners_world[:, 2])
+            box_world[0, 3] = np.max(box_corners_world[:, 0])
+            box_world[0, 4] = np.max(box_corners_world[:, 1])
+            box_world[0, 5] = np.max(box_corners_world[:, 2])
+            box_world[0, 6] = result["confidence"]
+            box_world[0, 7] = result["class_ID"]
+            boxes_world.append(box_world)
+            results_list.append(result)
+    boxes_world = np.concatenate(boxes_world)
+
+    pick = nms_3d_faster_samecls(boxes_world, nms_iou_cls)
+    assert (len(pick) > 0)
+    results_intermediate = [results_list[p] for p in pick]
+
+    nms_iou = 0.25  # this threshold is used to remove duplicates that are not of the same class
+    boxes_world = []
+    results_list = []
+    for result in results_intermediate:
+        if not result:
+            continue
+        box_world = np.zeros((1, 7), dtype=np.float32)
+        box_corners_world = result["box_corners_world"]
+        box_world[0, 0] = np.min(box_corners_world[:, 0])
+        box_world[0, 1] = np.min(box_corners_world[:, 1])
+        box_world[0, 2] = np.min(box_corners_world[:, 2])
+        box_world[0, 3] = np.max(box_corners_world[:, 0])
+        box_world[0, 4] = np.max(box_corners_world[:, 1])
+        box_world[0, 5] = np.max(box_corners_world[:, 2])
+        box_world[0, 6] = result["confidence"]
+        # box_world[0, 7] = result["class_ID"]
+        boxes_world.append(box_world)
+        results_list.append(result)
+    boxes_world = np.concatenate(boxes_world)
+
+    pick = nms_3d_faster(boxes_world, nms_iou)
+    assert (len(pick) > 0)
+
+    results_final = [results_list[p] for p in pick]
+    results_final = jsonify(results_final)
+
+    for r in results_final:
+        r['label_probs'] = [0] * len(class_list)
+        if r['class'] in class_list:
+            r['label_probs'][class_list.index(r['class'])] = r['confidence']
+
+    return results_final
+
+
+def jsonify(data_list):
+    json_list = []
+    for data in data_list:
+        json_data = dict()
+        for key, value in data.items():
+            if isinstance(value, list):  # for lists
+                value = [
+                    jsonify(item) if isinstance(item, dict) else item
+                    for item in value
+                ]
+            if isinstance(value, dict):  # for nested lists
+                value = jsonify(value)
+            if isinstance(key, int):  # if key is integer: > to string
+                key = str(key)
+            if type(
+                    value
+            ).__module__ == 'numpy':  # if value is numpy.*: > to python list
+                value = value.tolist()
+            json_data[key] = value
+        json_list.append(json_data)
+    return json_list
+
+
+def nms_3d_faster(boxes, overlap_threshold, old_type=False):
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    z1 = boxes[:,2]
+    x2 = boxes[:,3]
+    y2 = boxes[:,4]
+    z2 = boxes[:,5]
+    score = boxes[:,6]
+    area = (x2-x1)*(y2-y1)*(z2-z1)
+
+    I = np.argsort(score)
+    pick = []
+    while (I.size!=0):
+        last = I.size
+        i = I[-1]
+        pick.append(i)
+
+        xx1 = np.maximum(x1[i], x1[I[:last-1]])
+        yy1 = np.maximum(y1[i], y1[I[:last-1]])
+        zz1 = np.maximum(z1[i], z1[I[:last-1]])
+        xx2 = np.minimum(x2[i], x2[I[:last-1]])
+        yy2 = np.minimum(y2[i], y2[I[:last-1]])
+        zz2 = np.minimum(z2[i], z2[I[:last-1]])
+
+        l = np.maximum(0, xx2-xx1)
+        w = np.maximum(0, yy2-yy1)
+        h = np.maximum(0, zz2-zz1)
+
+        if old_type:
+            o = (l*w*h)/area[I[:last-1]]
+        else:
+            inter = l*w*h
+            o = inter / (area[i] + area[I[:last-1]] - inter)
+
+        I = np.delete(I, np.concatenate(([last-1], np.where(o>overlap_threshold)[0])))
+
+    return pick
+
+
+def nms_3d_faster_samecls(boxes, overlap_threshold, old_type=False):
+    x1 = boxes[:,0]
+    y1 = boxes[:,1]
+    z1 = boxes[:,2]
+    x2 = boxes[:,3]
+    y2 = boxes[:,4]
+    z2 = boxes[:,5]
+    score = boxes[:,6]
+    cls = boxes[:,7]
+    area = (x2-x1)*(y2-y1)*(z2-z1)
+
+    I = np.argsort(score)
+    pick = []
+    while (I.size!=0):
+        last = I.size
+        i = I[-1]
+        pick.append(i)
+
+        xx1 = np.maximum(x1[i], x1[I[:last-1]])
+        yy1 = np.maximum(y1[i], y1[I[:last-1]])
+        zz1 = np.maximum(z1[i], z1[I[:last-1]])
+        xx2 = np.minimum(x2[i], x2[I[:last-1]])
+        yy2 = np.minimum(y2[i], y2[I[:last-1]])
+        zz2 = np.minimum(z2[i], z2[I[:last-1]])
+        cls1 = cls[i]
+        cls2 = cls[I[:last-1]]
+
+        l = np.maximum(0, xx2-xx1)
+        w = np.maximum(0, yy2-yy1)
+        h = np.maximum(0, zz2-zz1)
+
+        if old_type:
+            o = (l*w*h)/area[I[:last-1]]
+        else:
+            inter = l*w*h
+            o = inter / (area[i] + area[I[:last-1]] - inter)
+        o = o * (cls1==cls2)
+
+        I = np.delete(I, np.concatenate(([last-1], np.where(o>overlap_threshold)[0])))
+
+    return pick
 
 def main():
 
-    pcd_path = "global_pcd.npy"
-    pcd = np.load(CLOUD_PATH + pcd_path)
-    vis_mm_2(pcd)
+    classes_sunrgbd = [
+        'bed', 'table', 'sofa', 'chair', 'toilet', 'desk', 'dresser',
+        'night_stand', 'bookshelf', 'bathtub'
+    ]
+    threshold = 0.9
+    all_results = []
+    model = init_model(CONFIG_FILE, CHCKPOINT_FILE)
+
+    for i in range(87):
+
+        filename = "0" * (6 - len(str(i))) + str(i)
+        print(f"Loading data: {filename}")
+        pcd_path = CLOUD_PATH + filename + ".npy"
+        depth_img, rgb_np, intrinsics, poses = load_data(filename)
+        pcd = create_pcd(depth_img, rgb_np, intrinsics, filename)
+
+
+        result, data = inference_detector(model, pcd_path)
+
+
+        #vis_mm(model, data, result, threshold)
+
+        camera_pose = poses['camera']
+        w_T_c = compute_world_T_camera(camera_pose['rotation_xyzw'], camera_pose['translation_xyz'])
+
+        all_results.append(analyze_result(result, w_T_c, threshold))
+
+    results = votenet_nms(all_results, classes_sunrgbd)
+    print("ok")
 
 if __name__ == "__main__":
     main()
